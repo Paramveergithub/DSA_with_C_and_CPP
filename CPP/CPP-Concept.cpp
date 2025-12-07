1eb9571563e32c9501f58abb48fe886a48d6873e
Paramveer Singh <negiparamveer7@gmail.com>
2025-12-07 12:43:25 +0530

    Day-55 (December-25) Questions

--- CPP/CPP-Concept.cpp
+++ CPP/CPP-Concept.cpp
@@ -310,4 +310,156 @@ int main(){
 // */
 
 
+// Question 271********Assignment-5_Question-2 ***************
+// Define a C++ function using the default argument that is able to add 2 or 3 numbers. 
+/*
+#include <iostream>
+using namespace std;
+int add(int a, int b, int c = 0){
+  return a+b+c;
+}
+int main(){
+  int a, b, c;
+  cout<<"Enter three numbers: ";
+  cin>>a>>b>>c;
+  cout<<"Sum of two numbers is : "<<add(a, b)<<endl;
+  cout<<"Sum of three numbers is : "<<add(a, b, c)<<endl;
+  return 0;
+}
+// */
+
+// Question 272********Assignment-5_Question-3 ***************
+// Define overloaded functions to calculate area of circle, area of rectangle and area of triangle
+/*
+#include <iostream>
+using namespace std;
+float area(int r){
+  return 3.14*r*r;
+}
+int area(int l, int b){
+  return l*b;
+}
+float area(int base, int h, float x){
+  return base*h*x;
+}
+int main(){
+  int r;
+  cout<<"Enter the radius of circle: ";
+  cin>>r;
+  cout<<"Area of circle is : "<< area(r)<<endl;
+  int length, breadth;
+  cout<<"Enter length and breadth of rectangle: ";
+  cin>>length>>breadth;
+  cout<<"Area of rectangle is : "<< area(length, breadth)<<endl;
+  int base, height;
+  cout<<"Enter base and height of triangle: ";
+  cin>>base>>height;
+  cout<<"Area of triangle is : "<< area(base, height, 0.5)<<endl;
+  return 0;
+}
+// */
+
+// Question 273********Assignment-5_Question-4 ***************
+// Write functions using function overloading to find a maximum of two numbers and both the numbers can be integer or real.
+/*
+#include <iostream>
+using namespace std;
+int max(int a, int b){
+  return (a>b)?a:b;
+}
+float max(float a, float b){
+  return (a>b)?a:b;
+}
+int main(){
+  int a, b;
+  cout<<"Enter two numbers: ";
+  cin>>a>>b;
+  cout<<max(a, b)<<" is greater"<<endl;
+  float x, y;
+  cout<<"Enter two real numbers: ";
+  cin>>x>>y;
+  cout<<max(x, y)<<" is greater"<<endl;
+  return 0;
+}
+// */
+
+// Question 274********Assignment-5_Question-5 ***************
+// Write functions using function overloading to add two numbers having different data types.
+/*
+#include <iostream>
+using namespace std;
+float add(int a, float b){
+  return a+b;
+}
+float add(float a, int b){
+  return a+b;
+}
+int main(){
+  int a;
+  float b;
+  cout<<"Enter an integer and a real number: ";
+  cin>>a>>b;
+  cout<<add(a, b)<<endl;
+  float x;
+  int y;
+  cout<<"Enter a real number and an integer: ";
+  cin>>x>>y;
+  cout<<add(x, y)<<endl;
+  return 0;
+}
+// */
+
+// Question 275********Assignment-6_Question-1 ***************
+// Define a C++ function to sort an array of integers in ascending or descending order depending on bool type argument (true for ascending and false for descending). Use default argument to implement it.
+/*
+#include <iostream>
+using namespace std;
+// void sortArray(int arr[], int n, bool order = false){
+void sortArray(int arr[], int n, bool order = true){
+  if(order){
+    for(int i=0; i<n-1; i++){
+      for(int j=0; j<n-i-1; j++){
+        if(arr[j]>arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
+      }
+    }
+    cout<<"Sorted Ascending array is: ";
+    for(int i=0; i<n; i++){
+      cout<<arr[i]<<" ";
+    }
+  }
+  else{
+    for(int i=0; i<n-1; i++){
+      for(int j=0; j<n-i-1; j++){
+        if(arr[j]<arr[j+1]){
+          int temp = arr[j];
+          arr[j] = arr[j+1];
+          arr[j+1] = temp;
+        }
+      }
+    }
+    cout<<"Sorted Descending array is: ";
+    for(int i=0; i<n; i++){
+      cout<<arr[i]<<" ";
+    }
+  }
+  cout<<endl;
+}
+int main(){
+  int n;
+  cout<<"Enter the size of array: ";
+  cin>>n;
+  int arr[n];
+  cout<<"Enter the elements of array: ";
+  for(int i=0; i<n; i++){
+    cin>>arr[i];
+  }
+  sortArray(arr, n, 1);
+  return 0;
+}
+// */
+
 
